fc465cc7ea6a62c60fa9fe8e978cbc9b17da5a65
mbaker <mbaker@MacBook-Pro.local>
2024-02-27 11:22:24 -0500

    add: mocked data sources

--- mock/src/components/REPL.tsx
+++ mock/src/components/REPL.tsx
@@ -24,7 +24,30 @@ export default function REPL() {
       {/* TODO: Update your REPLHistory and REPLInput to take in new shared state as props */}
       <REPLHistory history={history} verbose={verbose} />
       <hr></hr>
-      <REPLInput history={history} setHistory={setHistory} verbose={verbose} setVerbose={setVerbose} />
+      <REPLInput
+        history={history}
+        setHistory={setHistory}
+        verbose={verbose}
+        setVerbose={setVerbose}
+      />
     </div>
   );
 }
+
+const exampleCSV1 = [
+  [1, 2, 3, 4, 5],
+  ["The", "song", "remains", "the", "same."],
+];
+
+const exampleCSV2 = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+  [100, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+  [1000, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+  [10000, 2000, 30, 40, 50, 60, 70, 80, 90, 100],
+];
+
+const record = {
+  "datasource1.csv": exampleCSV1,
+  "datasource2.csv": exampleCSV2,
+};
