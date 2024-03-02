2fa239122115b3e7e32d453f22d94df064a04f5a
mbaker <mbaker@MacBook-Pro.local>
2024-03-02 16:13:15 -0500

    add: unit tests for view

--- mock/tests/unit/basic-example.spec.ts
+++ mock/tests/unit/basic-example.spec.ts
@@ -4,12 +4,13 @@
 
 // Test the functionality of the load, view, search functions directly
 
-import { expect, test } from 'vitest';
+import { expect, test } from "vitest";
 
 // all exports from main will now be available as main.X
 // import * as main from '../mock/src/main';
-import * as main from '../../src/main';
-import * as commands from '../../src/components/Commands';
+import * as main from "../../src/main";
+import * as commands from "../../src/components/Commands";
+import { datamocked } from "../../src/components/mockedJson";
 
 // test('is 1 + 1 = 2?', () => {
 //   expect(1 + 1).toBe(2)
@@ -23,12 +24,31 @@ import * as commands from '../../src/components/Commands';
 // // For more information on how to make unit tests, visit:
 // // https://jestjs.io/docs/using-matchers
 
-test('Load returns success with good filename', () =>{
-  expect(commands.load_csv("strings.csv")).toBe("Loading file at destination strings.csv")
-})
+test("Load returns success with good filename", () => {
+  expect(commands.load_csv("strings.csv")).toBe(
+    "Loading file at destination strings.csv"
+  );
+});
 
-test('Load returns fail with bad filename', () => {
-  expect(commands.load_csv("bad_string")).toBe("No file found at destination bad_string")
-})
+test("Load returns fail with bad filename", () => {
+  expect(commands.load_csv("bad_string")).toBe(
+    "No file found at destination bad_string"
+  );
+});
 
 // TODO: add more test cases
+test("Load returns fail with no filename", () => {
+  expect(commands.load_csv("")).toBe("Please provide filepath to load");
+});
+
+test("View returns success with loaded csv", () => {
+  let csv = commands.load_csv("more_strings.csv");
+  expect(commands.view_csv()).toBe(datamocked["more_strings.csv"]);
+});
+
+test("View returns fail with no loaded csv", () => {
+  expect(commands.load_csv("bad_file.csv")).toBe(
+    "No file found at destination bad_file.csv"
+  );
+  expect(commands.view_csv()).toBe("No csv loaded");
+});
